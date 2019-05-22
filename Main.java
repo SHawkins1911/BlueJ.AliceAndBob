45cdb90c660024f3455f00f6613fef07bd99d1ae
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-05-22 19:13:59 -0400

    Done

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
+    
     public static void main(String[] args ){
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String name = in.nextLine();
+        if(name.toLowerCase().equals("alice") || name.toLowerCase().equals("bob")){
+        System.out.println("Hello");
+        
+        };
+         
     }
 }
